5db54dd92dcc4c3c1370d4347f063de7daec8c65
WIN-17NV8CRBISB\user <cynvox@gmail.com>
2022-10-30 19:01:27 +0300

    add devTools

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { rootReducer } from './services/reducers.js'; 
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, );
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
